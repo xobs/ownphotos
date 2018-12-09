7bc0aceb6c6a15151abeea89d53ca4c5d4fecca9
Sean Cross <sean@xobs.io>
2018-12-09 22:15:29 +0800

    entrypoint: don't re-create the admin user
    If the admin user exists, simply update the password.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,13 +10,17 @@ source /venv/bin/activate
 python manage.py makemigrations api 2>&1 | tee logs/makemigrations.log
 python manage.py migrate 2>&1 | tee logs/migrate.log
 
+# Create a new user, or update the password if one exists.
 python manage.py shell <<EOF
 from api.models import User
-User.objects.filter(email='$ADMIN_EMAIL').delete()
-User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')
+users=User.objects.filter(email='$ADMIN_EMAIL')
+if len(users) > 0:
+    for user in users:
+        user.set_password('$ADMIN_PASSWORD')
+else:
+    User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')
 EOF
 
 echo "Running backend server..."
-
 python manage.py rqworker default 2>&1 | tee logs/rqworker.log &
 gunicorn --bind 0.0.0.0:8001 ownphotos.wsgi 2>&1 | tee logs/gunicorn.log
